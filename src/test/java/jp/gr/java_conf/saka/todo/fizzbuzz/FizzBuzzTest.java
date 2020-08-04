1f46eab90fbd3c33f429b405705f8dbf36ff96e3
sakata1222 <sakata1222@gmail.com>
2020-08-04 17:17:27 +0900

    refactor: refactor test class to describe specification

--- src/test/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzzTest.java
@@ -3,8 +3,11 @@ package jp.gr.java_conf.saka.todo.fizzbuzz;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("FizzBuzz class defines a convert rule of FizzBuzz problem")
 class FizzBuzzTest {
 
   private FizzBuzz fizzBuzz;
@@ -14,27 +17,43 @@ class FizzBuzzTest {
     fizzBuzz = new FizzBuzz();
   }
 
-  @Test
-  void convert_returns_1_as_string_when_1_is_specified() {
-    // execution and verification
-    assertThat(fizzBuzz.convert(1)).isEqualTo("1");
-  }
+  @Nested
+  class Convert_returns_a_string_when_a_number_is_specified {
 
-  @Test
-  void convert_returns_2_as_string_when_2_is_specified() {
-    // execution and verification
-    assertThat(fizzBuzz.convert(2)).isEqualTo("2");
-  }
+    @Nested
+    class Convert_returns_a_string_directly_if_the_number_is_not_divisible_3_or_5 {
 
-  @Test
-  void convert_returns_Fizz_when_3_is_specified() {
-    // execution and verification
-    assertThat(fizzBuzz.convert(3)).isEqualTo("Fizz");
-  }
+      @Test
+      void convert_returns_1_as_string_when_1_is_specified() {
+        // execution and verification
+        assertThat(fizzBuzz.convert(1)).isEqualTo("1");
+      }
 
-  @Test
-  void convert_returns_Buzz_when_5_is_specified() {
-    // execution and verification
-    assertThat(fizzBuzz.convert(5)).isEqualTo("Buzz");
+      @Test
+      void convert_returns_2_as_string_when_2_is_specified() {
+        // execution and verification
+        assertThat(fizzBuzz.convert(2)).isEqualTo("2");
+      }
+    }
+
+    @Nested
+    class Convert_returns_Fizz_when_a_number_is_divisible_by_3 {
+
+      @Test
+      void convert_returns_Fizz_when_3_is_specified() {
+        // execution and verification
+        assertThat(fizzBuzz.convert(3)).isEqualTo("Fizz");
+      }
+    }
+
+    @Nested
+    class Convert_returns_Buzz_when_a_number_is_divisible_by_5 {
+
+      @Test
+      void convert_returns_Buzz_when_5_is_specified() {
+        // execution and verification
+        assertThat(fizzBuzz.convert(5)).isEqualTo("Buzz");
+      }
+    }
   }
 }
