a5c52bcefaef2925dd4a58b86d6f1f22ab3c560f
sakata1222 <sakata1222@gmail.com>
2020-08-04 16:52:12 +0900

    feat: Just pass a Fizz test

--- src/main/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzz.java
+++ src/main/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,9 @@ package jp.gr.java_conf.saka.todo.fizzbuzz;
 public class FizzBuzz {
 
   public String convert(int number) {
+    if (number == 3) {
+      return "Fizz";
+    }
     return Integer.toString(number);
   }
 }
--- src/test/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzzTest.java
@@ -2,23 +2,33 @@ package jp.gr.java_conf.saka.todo.fizzbuzz;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+  private FizzBuzz fizzBuzz;
+
+  @BeforeEach
+  void prepareForEachTest() {
+    fizzBuzz = new FizzBuzz();
+  }
+
   @Test
-  void convert_1_to_1_as_string() {
-    // prepare
-    var fizzBuzz = new FizzBuzz();
+  void convert_returns_1_as_string_when_1_is_specified() {
     // execution and verification
     assertThat(fizzBuzz.convert(1)).isEqualTo("1");
   }
 
   @Test
-  void convert_2_to_2_as_string() {
-    // prepare
-    var fizzBuzz = new FizzBuzz();
+  void convert_returns_2_as_string_when_2_is_specified() {
     // execution and verification
     assertThat(fizzBuzz.convert(2)).isEqualTo("2");
+  }
+
+  @Test
+  void convert_returns_Fizz_when_3_is_specified() {
+    // execution and verification
+    assertThat(fizzBuzz.convert(3)).isEqualTo("Fizz");
   }
 }
