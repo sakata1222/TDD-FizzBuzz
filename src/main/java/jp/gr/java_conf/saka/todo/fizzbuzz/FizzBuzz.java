235036efddf46c36ba467ac58d84814b0d718dd2
sakata1222 <sakata1222@gmail.com>
2020-08-04 17:29:11 +0900

    refactor: FizzBuzz implement

--- src/main/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzz.java
+++ src/main/java/jp/gr/java_conf/saka/todo/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,17 @@ package jp.gr.java_conf.saka.todo.fizzbuzz;
 public class FizzBuzz {
 
   public String convert(int number) {
-    if (number % 3 == 0 && number % 5 == 0) {
-      return "FizzBuzz";
-    }
+    var converted = new StringBuilder();
     if (number % 3 == 0) {
-      return "Fizz";
+      converted.append("Fizz");
     }
     if (number % 5 == 0) {
-      return "Buzz";
+      converted.append("Buzz");
     }
-    return Integer.toString(number);
+    if (converted.length() != 0) {
+      return converted.toString();
+    } else {
+      return Integer.toString(number);
+    }
   }
 }
